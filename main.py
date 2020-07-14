503f2ba5c5d9b98bbae0c72d54ddfeb0c625bea7
Matteo De Leo <matteodeleo89@live.com>
2020-07-14 13:21:08 +0200

    Deployed base project structure

---
+++ main.py
@@ -0,0 +1,30 @@
+def load_cfg(yaml_filepath):
+    """
+    Load a YAML configuration file.    Parameters
+    ----------
+    yaml_filepath : str    Returns
+    -------
+    cfg : dict
+    """
+    # Read YAML experiment definition file
+    with open(yaml_filepath, 'r') as stream:
+        cfg = yaml.load(stream)
+    cfg = make_paths_absolute(os.path.dirname(yaml_filepath), cfg)
+    return cfgdef make_paths_absolute(dir_, cfg):
+    """
+    Make all values for keys ending with `_path` absolute to dir_.    Parameters
+    ----------
+    dir_ : str
+    cfg : dict    Returns
+    -------
+    cfg : dict
+    """
+    for key in cfg.keys():
+        if key.endswith("_path"):
+            cfg[key] = os.path.join(dir_, cfg[key])
+            cfg[key] = os.path.abspath(cfg[key])
+            if not os.path.isfile(cfg[key]):
+                logging.error("%s does not exist.", cfg[key])
+        if type(cfg[key]) is dict:
+            cfg[key] = make_paths_absolute(dir_, cfg[key])
+    return cfg
